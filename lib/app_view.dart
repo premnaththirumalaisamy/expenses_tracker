dd2c67b4d1237e6eb245407e85cd8b06c1d33c78
Premnath Thirumalaisamy <premnath.thirumalaisamy@sixt.com>
2024-04-01 14:56:49 +0530

    Initial commit

---
+++ lib/app_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'screens/home/views/home_screen.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "EXPENSE TRACKER",
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          background: Colors.grey.shade100,
+          onBackground: Colors.black,
+          primary: Color(0xFF00B2E7),
+          secondary: Color(0xFFE064F7),
+          tertiary: Color(0xFFFF8D6C)
+        )
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/home/views/home_screen.dart
@@ -0,0 +1,59 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.vertical(
+            top:Radius.circular(20)
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          elevation: 3,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.home),
+                label: "Home"
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.graph_square_fill),
+              label: "stats"
+            )
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(onPressed: () {  },
+        shape: CircleBorder(),
+      child: Container(
+        width: 60,
+        height: 60,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: LinearGradient(colors: [
+            Theme.of(context).colorScheme.tertiary,
+            Theme.of(context).colorScheme.secondary,
+            Theme.of(context).colorScheme.primary,
+          ],
+            transform: const GradientRotation(pi/4)
+          )
+        ),
+          child: const Icon(
+              CupertinoIcons.add,
+            color: Colors.white,
+            size: 30,
+          )
+      ),
+      ),
+    );
+  }
+}
